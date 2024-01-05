42fb9be247c434fa75f05a81b1903912768961ce
Kenneth Osei-Tutu Marfo <kobbie377@gmail.com>
2024-01-05 05:03:34 -0800

    Hard

--- python/licompre.py
+++ python/licompre.py
@@ -1,9 +1,14 @@
 numbers = [1,3,5,2,7,8,9,0,4,6]
 
-names = ["Kenneth", "leav", "Kee", "Peele"]
+firstnames = ["Kenneth", "leav", "Kee", "Peele"]
+othernames = ["Kenneth", "shah", "Kee","jdjsj", "jewjew", "iadjajd", "jajladk", "ksak"]
+
 
 newnumbers = [item*2 for item in numbers if item %2 ==0]
 
-newnames = [name.upper() for name in names]
+newnam = [name for name in othernames if name in firstnames]
+
+newnames = [name.upper() for name in firstnames]
 print(newnumbers)
 print(newnames)
+print(newnam)
