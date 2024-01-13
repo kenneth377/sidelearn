cae5f5b8c9450f09a328593fdadf71d57cc1c76f
kenneth377 <kobbie377@gmail.com>
2024-01-13 21:30:38 +0700

    linked lists

--- li.c
+++ li.c
@@ -1,11 +1,17 @@
-// Hard as usual. We move
-
+/* Hard as usual. We move
+Simple implementation of a singly linked-lists with addition and roemoval functions*/
 #include <stdio.h>
 
 
 struct Node* addnodeatbeginning(struct Node* h, struct Node* n);
-void addnodeaftertindex(struct Node* h, struct Node* n, int index);
+struct Node* addnodeaftertindex(struct Node* h, struct Node* n, int index);
 struct Node* addnodeatend(struct Node* h, struct Node* n);
+
+struct Node* removeatbeginning(struct Node* h);
+struct Node* removeaindex(struct Node* h, int index);
+struct Node* removeatend(struct Node* h);
+
+void printlist(struct Node* h);
 struct Node{
     struct Node* next;
     int data;
@@ -40,18 +46,37 @@ int main(){
     addnodeaftertindex(&head, &atindex, 0);
 
     struct Node* hi = addnodeatbeginning(&head, &atbeginning);
-    while(hi){
-        printf("%d\n",hi->data);
-        hi = hi->next;   
-    }
+   
+    printlist(hi);
 
+    hi = removeatbeginning(hi);
+    printlist(hi);
 
-    return 0;
+    hi = removeatend(hi);
+    printlist(hi);
+
+        return 0;
 
 }
 
 
+void printlist(struct Node* hi){
+    if(hi){
+        while(hi){
+            printf("%d\n",hi->data);
+            hi = hi->next;   
+        }
+    }
+    else{
+        printf("List is empty\n");
+        return;
+    }
+}
+
 struct Node* addnodeatend(struct Node* h, struct Node* n){
+     if(!h){
+        return NULL;
+    }
     struct Node* cu = h;
 
     while(cu->next){
@@ -65,14 +90,18 @@ struct Node* addnodeatend(struct Node* h, struct Node* n){
 
 struct Node* addnodeatbeginning(struct Node* h, struct Node* n){
     if(h){
-        // struct Node* nextnode = h->next
+
         n->next = h;
     }
 
     return n;
 }
 
-void addnodeaftertindex(struct Node* h, struct Node* n, int index){
+struct Node* addnodeaftertindex(struct Node* h, struct Node* n, int index){
+    struct Node* startnode =h; 
+     if(!h||!n||!index){
+        return NULL;
+    }
     int cuindex = 0;
 
     while(cuindex < index){
@@ -82,12 +111,63 @@ void addnodeaftertindex(struct Node* h, struct Node* n, int index){
         }
         else{
             printf("Index Error: Index does not exist in linked list\n");
-            return;
+            return NULL;
         }
     }
     struct Node* nodenext =h->next;
     h->next = n;
     n->next = nodenext;
 
-    return;
+    return startnode;
+}
+
+
+struct Node* removeatbeginning(struct Node* h){
+     if(!h){
+        return NULL;
+    }
+
+    struct Node* newhead = h->next;
+
+    h->next = NULL;
+    
+    return  newhead;
+}
+
+
+struct Node* removeatend(struct Node* h){
+    if(!h){
+        return NULL;
+    }
+    else if(!h->next){
+        return NULL;
+    }
+    struct Node* cu = h;
+    while(cu->next->next){
+        cu= cu->next;
+    }
+    cu->next =NULL;
+    return h;
+}
+
+struct Node* removeafterindex(struct Node* h, int index){
+    struct Node* startnode =h;
+    if(!h||!index){
+        return NULL;
+    }
+    int cu =0;
+
+    while(cu< index){
+        if(h->next){
+            h=h->next;
+            cu+=1; 
+        }
+        else{
+            printf("Index Error:Index does not exist in linked list ");
+            return NULL;
+        }  
+    }
+
+    h->next =NULL;
+    return startnode;
 }
